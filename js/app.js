2b53d0cec6ee0dd8b530b81e42a375f3132a93c7
Joaquin Paravecino <joaquinparavecino17@gmail.com>
2022-11-18 17:33:40 -0500

    Entregable 3

--- js/app.js
+++ js/app.js
@@ -1,45 +1,83 @@
 const saludo =document.querySelector(".hero__text h4")
-const nombreUsuario = prompt("Para una mejor experiencia , por favor ingresar su nombre");
+const nombreUsuario =  prompt("Para una mejor experiencia , ingrese sus datos por")
+
 saludo.innerText = `Un placer atenderte ${nombreUsuario}, bienvenid@ y espero disfrutes en....`;
 
 
-const boton1 =document.querySelector(".guy1")
-
-boton1. addEventListener("click" , () => {
-    alert("Excelente decision , tu toxica estara feliz");
-})
-
-const boton2 =document.querySelector(".guy2")
-
-boton2. addEventListener("click" , () => {
-    alert("Excelente decision , tu novia estara feliz");
-})
-
-const boton3 =document.querySelector(".guy3")
-
-boton3. addEventListener("click" , () => {
-    alert("Excelente decision ,  tu chica inocente estara feliz");
-})
 
 const formulario =document.querySelector("#form")
 formulario.addEventListener("click", () => {
-    alert("Su mesa esta reservada , lo esperamos con mucho gusto ")
+    Swal.fire(
+        'Excelente',
+        'Muy pronto recibira informacion de nuestras ofertas',
+        
+      )
 })
 
 
 const cardContainer = document.querySelector("#cards")
+const contenedorCarrito=document.getElementById("carrito-contenedor")
+const contadorCarrito =document.getElementById("contadorCarrito")
+
+
+//Carrito
+let carrito =[]
+
+document.addEventListener("DOMContentLoaded",() => {
+    if(localStorage.getItem("carrito")){
+        carrito = JSON.parse(localStorage.getItem("carrito"))
+        actualizarCarrito()
+    }
+})
 
 nuevosProductos.forEach((_producto) => {
-    const nuevaCard = document.createElement("div")
+    const nuevaCard = document.createElement(`div`)
 
-    nuevaCard.className = "card"
+    nuevaCard.className = `card`
     nuevaCard.innerHTML = `
     <img src=${_producto.imgSrc}alt="burguer" />
           <h4>${_producto.producto}</h4>
-          <p>${_producto.caracter}</p>
-          <button class="guy1"> Comprar ahora</button> 
+          <p><span> S/${_producto.precio}.00</span></p>
+          <button id="agregar${_producto.id}" class ="boton-agregar">Comprar ahora <i class="fa-light fa-cart-shopping"></i </button> 
 
     `
     cardContainer.append(nuevaCard)
-   
+    
+
+   const boton = document.getElementById(`agregar${_producto.id}`)
+   boton.addEventListener("click",() => {
+    agregarAlcarrito(_producto.id)
+
+   })
+boton.addEventListener("click",()=>{
+    Swal.fire(
+        {
+         title:"Muchas gracias por su compra",
+         background:"linear-gradient(to right, #f83600 0%, #f9d423 100%)",
+          customClass:{
+           title:"titulo",
+    
+          },
+          icon:"success"
+        }
+      )
 })
+
+})
+
+const agregarAlcarrito = (prodId) =>{
+    const item = nuevosProductos.find((prod) => prod.id ===prodId)
+    carrito.push(item)
+    actualizarCarrito()
+   
+}
+
+const actualizarCarrito = () => {
+    contenedorCarrito.innerHTML = ""
+    carrito.forEach((_producto) => {
+        localStorage.setItem("carrito",JSON.stringify(carrito))
+      
+        
+    })
+    contadorCarrito.innerText = carrito.length
+}
--- js/data.js
+++ js/data.js
@@ -1,13 +1,17 @@
-class hamburguesas {
-   constructor (producto, imgSrc ,caracter){
-   this.producto=producto
-   this.imgSrc=imgSrc
-   this.caracter=caracter
-   
-   }
-}
 
-const hamburguesa1= new hamburguesas("la ´No tengo hambre´ ","https://3.bp.blogspot.com/-CUjCXfbdviw/UQmkwcAk1bI/AAAAAAAABzk/ghzdu-oiR4g/s1600/carne+hamburguesa.jpg","Esta hamburguesa esta especialmente hecha para las que siempre tienen hambre")
-const hamburguesa2= new hamburguesas("la ´No se tu sabras´ "," https://static3.depositphotos.com/1000691/109/i/950/depositphotos_1098427-stock-photo-cheeseburger.jpg","Esta hamburguesa esta especialmente hecha para las que creen que leemos mentes ")
-const hamburguesa3 = new hamburguesas("la ´Es solo un amigo´ "," https://st2.depositphotos.com/3325813/8444/i/950/depositphotos_84445072-stock-photo-hamburger-sandwich-burger-with-cheese.jpg"," Esta hamburguesa si es muy muy especial, es para todas , las que dicen ´es solo un amigo´")
-const nuevosProductos =[hamburguesa1,hamburguesa2,hamburguesa3]
+
+const nuevosProductos=[
+
+{id:1,producto : "La `Toxica`",imgSrc:"https://3.bp.blogspot.com/-CUjCXfbdviw/UQmkwcAk1bI/AAAAAAAABzk/ghzdu-oiR4g/s1600/carne+hamburguesa.jpg",precio: 25},
+{id:2,producto : "La `No se eligue tu`",imgSrc:"https://3.bp.blogspot.com/-CUjCXfbdviw/UQmkwcAk1bI/AAAAAAAABzk/ghzdu-oiR4g/s1600/carne+hamburguesa.jpg",precio: 25},
+{id:3,producto : "La `Inocente`",imgSrc:"https://3.bp.blogspot.com/-CUjCXfbdviw/UQmkwcAk1bI/AAAAAAAABzk/ghzdu-oiR4g/s1600/carne+hamburguesa.jpg",precio: 25},
+{id:4,producto : "La `No tengo hambre`",imgSrc:"https://3.bp.blogspot.com/-CUjCXfbdviw/UQmkwcAk1bI/AAAAAAAABzk/ghzdu-oiR4g/s1600/carne+hamburguesa.jpg",precio: 25},
+{id:5,producto : "La `No se tu sabras`",imgSrc:"https://3.bp.blogspot.com/-CUjCXfbdviw/UQmkwcAk1bI/AAAAAAAABzk/ghzdu-oiR4g/s1600/carne+hamburguesa.jpg",precio: 25},
+{id:6,producto : "La `Es solo un amigo`",imgSrc:"https://3.bp.blogspot.com/-CUjCXfbdviw/UQmkwcAk1bI/AAAAAAAABzk/ghzdu-oiR4g/s1600/carne+hamburguesa.jpg",precio: 25},
+
+
+
+
+
+
+]
