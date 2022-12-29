9a4ef6db7767c8b9429e21e222850ba8f3dce69a
Joaquin Paravecino <joaquinparavecino17@gmail.com>
2022-12-29 10:33:54 -0500

    correccion

--- js/app.js
+++ js/app.js
@@ -11,12 +11,21 @@ formulario.addEventListener("click", () => {
 
 
 const cardContainer = document.querySelector("#cards")
-const contenedorCarrito=document.getElementById("carrito-contenedor")
+const contenedorCarrito=document.getElementById(`carrito-compras`)
+const botonVaciar = document.getElementById('vaciar-carrito')
 const contadorCarrito =document.getElementById("contadorCarrito")
+const cantidad = document.getElementById('cantidad')
+const precioTotal = document.getElementById('precioTotal')
+const cantidadTotal = document.getElementById('cantidadTotal')
 
 
 //Carrito
 let carrito =[]
+
+botonVaciar.addEventListener('click', () => {
+    carrito.length = 0
+    actualizarCarrito()
+})
 
 document.addEventListener("DOMContentLoaded",() => {
     if(localStorage.getItem("carrito")){
@@ -33,22 +42,23 @@ nuevosProductos.forEach((_producto) => {
     <img src=${_producto.imgSrc}alt="burguer" />
           <h4>${_producto.producto}</h4>
           <p><span> S/${_producto.precio}.00</span></p>
-          <button id="agregar${_producto.id}" class ="boton-agregar">Comprar ahora <i class="fa-light fa-cart-shopping"></i </button> 
+          <button id="agregar${_producto.id}" class ="boton-agregar">Comprar ahora </button> 
 
     `
     cardContainer.append(nuevaCard)
     
 
    const boton = document.getElementById(`agregar${_producto.id}`)
-   boton.addEventListener("click",() => {
-    agregarAlcarrito(_producto.nuevosProductos)
+
+   boton.addEventListener("click", () => {
+    agregarAlcarrito(_producto.id)
 
    })
    // Alert de compras
 boton.addEventListener("click",()=>{
     Swal.fire(
         {
-         title:"Muchas gracias por su compra",
+         title:"Muchas gracias, se agrego tu producto al carrito",
          background:"linear-gradient(to right, #f83600 0%, #f9d423 100%)",
           customClass:{
            title:"titulo",
@@ -61,19 +71,43 @@ boton.addEventListener("click",()=>{
 
 })
 
-const agregarAlcarrito = (prodId) =>{
-    const item = nuevosProductos.find((prod) => prod.id ===prodId)
+const agregarAlcarrito = (prodId) => {
+    const item = nuevosProductos.find((prod) => prod.id === prodId)
     carrito.push(item)
     actualizarCarrito()
+
    
 }
 
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId)
+
+    const indice = carrito.indexOf(item) 
+
+    carrito.splice(indice, 1)  
+    actualizarCarrito() }
+
+
+
 const actualizarCarrito = () => {
-    contenedorCarrito.innerHTML = ""
-    carrito.forEach((_producto) => {
+     contenedorCarrito.innerHTML=""
+
+    carrito.forEach((prod) => {
+        const div = document.createElement("div")
+        div.className = ("productoEnCarrito")
+        div.innerHTML=`
+        <p>${prod.producto}</p>
+        <p>Precio:S/ ${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        
+        `
+        contenedorCarrito.appendChild(div)
+
         localStorage.setItem("carrito",JSON.stringify(carrito))
       
         
     })
     contadorCarrito.innerText = carrito.length
+    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
 }
--- js/data.js
+++ js/data.js
@@ -2,12 +2,12 @@
 
 const nuevosProductos=[
 
-{id:1,producto : "La `Toxica`",imgSrc:"https://3.bp.blogspot.com/-CUjCXfbdviw/UQmkwcAk1bI/AAAAAAAABzk/ghzdu-oiR4g/s1600/carne+hamburguesa.jpg",precio: 25},
-{id:2,producto : "La `No se eligue tu`",imgSrc:"https://3.bp.blogspot.com/-CUjCXfbdviw/UQmkwcAk1bI/AAAAAAAABzk/ghzdu-oiR4g/s1600/carne+hamburguesa.jpg",precio: 25},
-{id:3,producto : "La `Inocente`",imgSrc:"https://3.bp.blogspot.com/-CUjCXfbdviw/UQmkwcAk1bI/AAAAAAAABzk/ghzdu-oiR4g/s1600/carne+hamburguesa.jpg",precio: 25},
-{id:4,producto : "La `No tengo hambre`",imgSrc:"https://3.bp.blogspot.com/-CUjCXfbdviw/UQmkwcAk1bI/AAAAAAAABzk/ghzdu-oiR4g/s1600/carne+hamburguesa.jpg",precio: 25},
-{id:5,producto : "La `No se tu sabras`",imgSrc:"https://3.bp.blogspot.com/-CUjCXfbdviw/UQmkwcAk1bI/AAAAAAAABzk/ghzdu-oiR4g/s1600/carne+hamburguesa.jpg",precio: 25},
-{id:6,producto : "La `Es solo un amigo`",imgSrc:"https://3.bp.blogspot.com/-CUjCXfbdviw/UQmkwcAk1bI/AAAAAAAABzk/ghzdu-oiR4g/s1600/carne+hamburguesa.jpg",precio: 25},
+{id: 1, producto : "La `Toxica`",imgSrc:"https://3.bp.blogspot.com/-CUjCXfbdviw/UQmkwcAk1bI/AAAAAAAABzk/ghzdu-oiR4g/s1600/carne+hamburguesa.jpg",precio: 25, cantidad: 1},
+{id: 2, producto : "La `No se eligue tu`",imgSrc:"https://3.bp.blogspot.com/-CUjCXfbdviw/UQmkwcAk1bI/AAAAAAAABzk/ghzdu-oiR4g/s1600/carne+hamburguesa.jpg",precio: 25,cantidad:  1},
+{id: 3, producto : "La `Inocente`",imgSrc:"https://3.bp.blogspot.com/-CUjCXfbdviw/UQmkwcAk1bI/AAAAAAAABzk/ghzdu-oiR4g/s1600/carne+hamburguesa.jpg",precio: 25, cantidad: 1},
+{id: 4, producto : "La `No tengo hambre`",imgSrc:"https://3.bp.blogspot.com/-CUjCXfbdviw/UQmkwcAk1bI/AAAAAAAABzk/ghzdu-oiR4g/s1600/carne+hamburguesa.jpg",precio: 25 ,cantidad: 1},
+{id: 5, producto : "La `No se tu sabras`",imgSrc:"https://3.bp.blogspot.com/-CUjCXfbdviw/UQmkwcAk1bI/AAAAAAAABzk/ghzdu-oiR4g/s1600/carne+hamburguesa.jpg",precio: 25,cantidad: 1},
+
 
 
 
